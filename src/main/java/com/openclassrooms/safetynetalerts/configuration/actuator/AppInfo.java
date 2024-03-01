f56abacb9c3594085ac0fc4d62bded8069fe39d2
The-Great-Toad <bearcode26@gmail.com>
2024-03-01 16:41:08 +0100

    tech(UT): Actuator configuration

---
+++ src/main/java/com/openclassrooms/safetynetalerts/configuration/actuator/AppInfo.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.safetynetalerts.configuration.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class AppInfo implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, Object> details = new HashMap<>();
+        details.put("name","SafetyNet Alerts");
+        details.put("description","Le but de cet application est d'envoyer des informations aux systèmes de services d'urgence.");
+        details.put("version","v1.0.0");
+        builder.withDetails(details);
+    }
+}
